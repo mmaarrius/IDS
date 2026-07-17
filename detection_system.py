ad1d9990e988341a2b08ebe7833d72eefcf9989f
Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>
2026-07-17 13:27:19 +0300

    Added the detectionEngine class with the constructor in the dectection_system.py file.
    Signed-off-by: Elena Smarandoiu <smarandoiuelenaandrada@gmail.com>

--- detection_system.py
+++ detection_system.py
@@ -1,2 +1,14 @@
-# This is where the detection system will be implemented
-# Rules and code... to be continued
+from sklearn.neighbors import LocalOutlierFactor
+import numpy as nm
+
+class detectionEngine:
+    
+    def __init__(self):
+        
+        self.anomaly_detector = LocalOutlierFactor( n_neighbors=20, contamination=0.1, novelty=True)
+        self.is_trained = False
+        
+        self.baseline_stats = {}
+        self.signature_rules = self.load_signature_rules()
+        self.training_data = []
+    
